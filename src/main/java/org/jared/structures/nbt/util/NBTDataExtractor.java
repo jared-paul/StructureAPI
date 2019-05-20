379d080ba20e5170632a1935598e9166ee718806
BuildTools <unconfigured@null.spigotmc.org>
2019-05-19 21:26:15 -0700

    Remove unneeded class

--- src/main/java/org/jared/structures/nbt/util/NBTDataExtractor.java
+++
@@ -1,9 +0,0 @@
-package org.jared.structures.nbt.util;
-
-import org.bukkit.Material;
-import org.bukkit.block.data.BlockData;
-import org.jared.structures.nbt.*;
-
-import java.util.Iterator;
-import java.util.Map;
-
